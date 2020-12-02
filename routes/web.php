73b5c8f5a06aea4109eb99e1714766c760f1bb89
Isaac Rovira <isaac.rovira.rocasalbas@gmail.com>
2020-12-02 10:49:50 +0100

    Incio del Producto 3. (Laravel 8) TAG v0.1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function(){
+    $controller = request('controller');
+    $method = request('method');
+    return view('hola', ['controller'=>$controller, 'method'=> $method]);
+    });
+
+//Willcards
